c614faf099e9dd70a1581a85b4475832e1d1db89
213-Bhavyasree <sreebhavya548@gmail.com>
2022-02-03 16:02:58 +0530

    Add project files.

---
+++ ConsumeApiToAngularStudent/Data/ConsumeApiToAngularStudentContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ConsumeApiToAngularStudent.Model;
+
+namespace ConsumeApiToAngularStudent.Data
+{
+    public class ConsumeApiToAngularStudentContext : DbContext
+    {
+        public ConsumeApiToAngularStudentContext (DbContextOptions<ConsumeApiToAngularStudentContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ConsumeApiToAngularStudent.Model.Student> Student { get; set; }
+    }
+}
